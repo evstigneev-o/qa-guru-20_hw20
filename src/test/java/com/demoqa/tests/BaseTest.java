4e837ff20e481fa97f75fc084540d1e848037c4a
Oleg Evstigneev <o.evstigneev@citydrive.ru>
2023-08-15 14:12:49 +0300

    small fixes after review

--- src/test/java/com/demoqa/tests/BaseTest.java
+++ src/test/java/com/demoqa/tests/BaseTest.java
@@ -1,13 +1,10 @@
 package com.demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
-import com.demoqa.api.AuthorizationApi;
-import com.demoqa.api.BooksApi;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
 
 public class BaseTest {
-
     @BeforeAll
     static void setup() {
         Configuration.baseUrl = "https://demoqa.com";
